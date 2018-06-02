c39193cc6ce14a5206a4deadb16cc8f0eb9cb191
okokanya <okokanya@gmail.com>
2018-06-02 17:29:56 +0200

    formating changed

--- game.js
+++ game.js
@@ -1,19 +1,19 @@
 'use strict';
 
 class Vector {
-	constructor(x = 0,y = 0) {
+	constructor(x = 0, y = 0) {
 		this.x = x;
-		this.y = y
-	  }
+		this.y = y;
+	}
 	plus(newVector) {
 		if (!(newVector instanceof Vector)) {
-			throw new Error ('Можно прибавлять к вектору только вектор типа Vector');
+			throw new Error('Можно прибавлять к вектору только вектор типа Vector');
 		}
 		return new Vector(this.x + newVector.x, this.y + newVector.y);
 	}
- 	times(number) {
- 		return new Vector(this.x * number, this.y * number);
- 	}
+	times(number) {
+		return new Vector(this.x * number, this.y * number);
+	}
 }
 
 const start = new Vector(30, 50);
@@ -24,61 +24,74 @@ console.log(`Исходное расположение: ${start.x}:${start.y}`);
 console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
 
 class Actor {
-  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-    if (!(pos instanceof Vector & size instanceof Vector & speed instanceof Vector)) {
-      throw new Error("Можно передать только вектор типа Vector");
-    }
-    this.pos = pos;
-    this.size = size;
-    this.speed = speed;
-  }
-  act() {}
+	constructor(
+		pos = new Vector(0, 0),
+		size = new Vector(1, 1),
+		speed = new Vector(0, 0)
+	) {
+		if (
+			!(
+				(pos instanceof Vector) &
+				(size instanceof Vector) &
+				(speed instanceof Vector)
+			)
+		) {
+			throw new Error('Можно передать только вектор типа Vector');
+		}
+		this.pos = pos;
+		this.size = size;
+		this.speed = speed;
+	}
+	act() {}
 
-  get left() {
-    return this.pos.x;
-  }
+	get left() {
+		return this.pos.x;
+	}
 
-  get right() {
-    return this.pos.x + this.size.x;
-  }
+	get right() {
+		return this.pos.x + this.size.x;
+	}
 
-  get top() {
-    return this.pos.y;
-  }
+	get top() {
+		return this.pos.y;
+	}
 
-  get bottom() {
-    return this.pos.y + this.size.y;
-  }
+	get bottom() {
+		return this.pos.y + this.size.y;
+	}
 
-  get type() {
-    return 'actor';
-  }
+	get type() {
+		return 'actor';
+	}
 
-  isIntersect(item) {
-    if (!(item instanceof Actor)) {
-      throw new Error("Можно передать только объект типа Actor");
-    }
-    if (item === this) {
-      return false;
-    }
-    return this.top < item.bottom && this.bottom > item.top && this.left < item.right && this.right > item.left
-  }
+	isIntersect(item) {
+		if (!(item instanceof Actor)) {
+			throw new Error('Можно передать только объект типа Actor');
+		}
+		if (item === this) {
+			return false;
+		}
+		return (
+			this.top < item.bottom &&
+			this.bottom > item.top &&
+			this.left < item.right &&
+			this.right > item.left
+		);
+	}
 }
 
-
-// ** пример использования 
-
+// ** пример использования
 
 // const items = new Map();
 // const player = new Actor();
 // items.set('Игрок', player);
 // items.set('Первая монета', new Actor(new Vector(10, 10)));
 // items.set('Вторая монета', new Actor(new Vector(15, 5)));
-    
+
 // function position(item) {
 //   return ['left', 'top', 'right', 'bottom']
 //     .map(side => `${side}: ${item[side]}`)
-//     .join(', ');  
+//     .join(', ');
 // }
 
 // function movePlayer(x, y) {
@@ -100,71 +113,73 @@ class Actor {
 
 // ----------
 class Level {
-  constructor(grid = [], actors = []) {
-    this.grid = grid;
-    this.actors = actors;
-    this.player = this.actors.find(function(actor) {
-      return actor.type === 'player';
-    });
+	constructor(grid = [], actors = []) {
+		this.grid = grid;
+		this.actors = actors;
+		this.player = this.actors.find(function(actor) {
+			return actor.type === 'player';
+		});
 
-    if (typeof(grid) !== 'undefined') {
-      this.height = grid.length;
-      this.width = Math.max(...grid.map(function(arr) {
-        return arr.length;
-      }));
-    } else {
-      this.height = 0;
-      this.width = 0;
-    }
-    this.status = null;
-    this.finishDelay = 1;
-  }
+		if (typeof grid !== 'undefined') {
+			this.height = grid.length;
+			this.width = Math.max(
+				...grid.map(function(arr) {
+					return arr.length;
+				})
+			);
+		} else {
+			this.height = 0;
+			this.width = 0;
+		}
+		this.status = null;
+		this.finishDelay = 1;
+	}
 
-  isFinished() {
+	isFinished() {
 		return this.status !== null && this.finishDelay < 0;
-  }
+	}
 	actorAt(newActor) {
 		if (!(newActor instanceof Actor)) {
 			throw new Error('аргумент - объект типа Actor');
 		}
 		return this.actors.find(elem => newActor.isIntersect(elem));
-  }
-  
-  obstacleAt(pos, size) {
-    if (!(pos instanceof Vector) || !(size instanceof Vector)) {
-      throw new Error('Тип аргумента - Vector');
-    }
+	}
 
-    let actor = new Actor(pos, size);
+	obstacleAt(pos, size) {
+		if (!(pos instanceof Vector) || !(size instanceof Vector)) {
+			throw new Error('Тип аргумента - Vector');
+		}
 
-    if (actor.bottom >= this.height) {
-      return 'lava';
-    }
-    if (actor.left < 0 || actor.top < 0 || actor.right >= this.width) {
-      return 'wall';
-    }
-    if (typeof(this.grid) === 'undefined') {
-      return;
-    }
-    for (let y = Math.floor(actor.top); y < Math.ceil(actor.bottom); y++) {
-      for (let x = Math.floor(actor.left); x < Math.ceil(actor.right); x++) {
-        if (typeof(this.grid[x][y] !== 'undefined')) {
-          return (this.grid)[x][y];
-        }
-      }
-    }
-  }
+		let actor = new Actor(pos, size);
 
-  removeActor(actor) {
+		if (actor.bottom >= this.height) {
+			return 'lava';
+		}
+		if (actor.left < 0 || actor.top < 0 || actor.right >= this.width) {
+			return 'wall';
+		}
+		if (typeof this.grid === 'undefined') {
+			return;
+		}
+		for (let y = Math.floor(actor.top); y < Math.ceil(actor.bottom); y++) {
+			for (let x = Math.floor(actor.left); x < Math.ceil(actor.right); x++) {
+				if (typeof (this.grid[x][y] !== 'undefined')) {
+					return this.grid[x][y];
+				}
+			}
+		}
+	}
+
+	removeActor(actor) {
 		this.actors = this.actors.filter(item => item !== actor);
 	}
 
 	noMoreActors(type) {
 		return !this.actors.some(item => item.type === type);
-  }
+	}
 
-  playerTouched(typeString, actorTouch) {
-		if(this.status !== null) { 
+	playerTouched(typeString, actorTouch) {
+		if (this.status !== null) {
 			return;
 		}
 
@@ -172,25 +187,21 @@ class Level {
 			this.status = 'lost';
 		}
 
-		if(typeString === 'coin') {
+		if (typeString === 'coin') {
 			this.removeActor(actorTouch);
-			if(this.noMoreActors('coin')) {
+			if (this.noMoreActors('coin')) {
 				this.status = 'won';
 			}
 		}
 	}
 }
 
-
-// ** пример использования 
-const grid = [
-  [undefined, undefined],
-  ['wall', 'wall']
-];
+// ** пример использования
+const grid = [[undefined, undefined], ['wall', 'wall']];
 
 function MyCoin(title) {
-  this.type = 'coin';
-  this.title = title;
+	this.type = 'coin';
+	this.title = title;
 }
 MyCoin.prototype = Object.create(Actor);
 MyCoin.constructor = MyCoin;
@@ -200,22 +211,22 @@ const bronzeCoin = new MyCoin('Бронза');
 const player = new Actor();
 const fireball = new Actor();
 
-const level = new Level(grid, [ goldCoin, bronzeCoin, player, fireball ]);
+const level = new Level(grid, [goldCoin, bronzeCoin, player, fireball]);
 
 level.playerTouched('coin', goldCoin);
 level.playerTouched('coin', bronzeCoin);
 
 if (level.noMoreActors('coin')) {
-  console.log('Все монеты собраны');
-  console.log(`Статус игры: ${level.status}`);
+	console.log('Все монеты собраны');
+	console.log(`Статус игры: ${level.status}`);
 }
 
 const obstacle = level.obstacleAt(new Vector(1, 1), player.size);
 if (obstacle) {
-  console.log(`На пути препятствие: ${obstacle}`);
+	console.log(`На пути препятствие: ${obstacle}`);
 }
 
 const otherActor = level.actorAt(player);
 if (otherActor === fireball) {
-  console.log('Пользователь столкнулся с шаровой молнией');
+	console.log('Пользователь столкнулся с шаровой молнией');
 }
